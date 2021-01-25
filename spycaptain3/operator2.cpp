b2c8a2ad5801b3e7caa07cc56f0617daca0606f4
Deepak Gupta <77580498+spycaptain3@users.noreply.github.com>
2021-01-25 17:28:35 +0530

    Create operator2.cpp

---
+++ spycaptain3/operator2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>   // formatting floating-point numbers with 1 decimal place
+using namespace std;
+ 
+int main() 
+{
+   int m1 = 5, m2 = 7;
+   double d1 = 3.7, d2 = 8.0;
+   
+   cout << fixed << setprecision(1);  
+ 
+   cout <<" "<< m1 << " + " << m2 << " = " << m1+m2 << endl;  
+   cout <<" "<< d1 << " + " << d2 << " = " << d1+d2 << endl;  
+   cout <<" "<< m1 << " + " << d2 << " = " << m1+d2 << endl;  
+ 
+   cout <<" "<< m1 << " - " << m2 << " = " << m1-m2 << endl;  
+   cout <<" "<< d1 << " - " << d2 << " = " << d1-d2 << endl;  
+   cout <<" "<< m1 << " - " << d2 << " = " << m1-d2 << endl;  
+ 
+   cout <<" "<< m1 << " * " << m2 << " = " << m1*m2 << endl; 
+   cout <<" "<< d1 << " * " << d2 << " = " << d1*d2 << endl;  
+   cout <<" "<< m1 << " * " << d2 << " = " << m1*d2 << endl;  
+ 
+   cout <<" "<< m1 << " / " << m2 << " = " << m1/m2 << endl;  
+   cout <<" "<< d1 << " / " << d2 << " = " << d1/d2 << endl;  
+   cout <<" "<< m1 << " / " << d2 << " = " << m1/d2 << endl;  
+   cout << endl;
+   return 0;
+}
